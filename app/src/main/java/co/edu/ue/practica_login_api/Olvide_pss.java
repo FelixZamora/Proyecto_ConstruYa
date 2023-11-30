1624dd4c80c233acd0cad6611d426b0b3ebe432d
Felix Ruiz <fruiz@uniempresarial.edu.co>
2023-11-30 11:00:08 -0500

    Primer_commit

---
+++ app/src/main/java/co/edu/ue/practica_login_api/Olvide_pss.java
@@ -0,0 +1,14 @@
+package co.edu.ue.practica_login_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Olvide_pss extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvide_pss);
+    }
+}
---
+++ app/src/main/java/co/edu/ue/practica_login_api/Terminos_condiciones.java
@@ -0,0 +1,14 @@
+package co.edu.ue.practica_login_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Terminos_condiciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminos_condiciones);
+    }
+}
