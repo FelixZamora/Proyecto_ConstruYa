4158db9b774e3d8ee2b4fd9ce91135684d06ca2e
Felix Ruiz <fruiz@uniempresarial.edu.co>
2023-12-04 15:59:41 -0500

    Commit hoy 04/12/2023

--- app/src/main/java/co/edu/ue/practica_login_api/api/ValuesApi.java
+++ app/src/main/java/co/edu/ue/practica_login_api/api/ValuesApi.java
@@ -3,4 +3,5 @@ package co.edu.ue.practica_login_api.api;
 public interface ValuesApi {
     //String BASE_URL = "http://10.57.30.229/api_12b/";
     String BASE_URL = "http://192.168.1.8/api_12b/";
+
 }
